120b11bca7939c7f7e2104f11949ead8187dccd0
Belén María León <belenleon10.98@gmail.com>
2023-06-20 08:37:17 +0200

    Refactorizacion 1 Gato - Belen Maria Leon Fernandez

--- Gato.py
+++ Gato.py
@@ -1,11 +1,16 @@
 """
 Clase Gato.
 :author: Jaime Rabasco Ronda.
+:author: Belen Maria Leon Fernandez
 """
 class Gato:
 
     def maullar(self):
-        print('Miau');
+        print(self.maulla());
+
+    def maulla(self):
+        return 'Miau'
+
 
 g = Gato();
 g.maullar();
